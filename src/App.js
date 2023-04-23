d672fb453daa75b4b9a72a4fc2b8dacd0b60a1c5
Dan846 <danilam846@gmail.com>
2023-04-23 23:48:35 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PostsProvider from './Components/PostsProvider';
+import HomePage from './Pages/HomePage';
+import CreatePost from './Pages/CreatePost';
+import ViewPost from './Pages/ViewPost';
+import EditPost from './Pages/EditPost';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<PostsProvider>
+				<Router>
+					<Routes>
+						<Route path='/' exact element={<HomePage />} ></Route>
+						<Route path='/posts/new' element={<CreatePost />} ></Route>
+						<Route path='/posts/edit/:id' element={<EditPost />} ></Route>
+						<Route path='/posts/:id' element={<ViewPost />} ></Route>
+					</Routes>
+				</Router>
+			</PostsProvider>
+		</div>
+	);
 }
 
 export default App;
